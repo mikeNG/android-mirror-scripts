4e7d7154f24380cb57bb1baaabe204fc15ea2448
Michael Bestas <mikeioannina@gmail.com>
2022-11-16 21:29:45 +0200

    Don't update repo during sync
    repo is known to have broken releases, which breaks sync
    and requires manual intervention to fix.

--- sync.sh
+++ sync.sh
@@ -4,10 +4,6 @@ MIRROR_ROOT=${MIRROR_ROOT:=/mnt/mirrors}
 
 # Sync the scripts and required manifests
 pushd ${MIRROR_ROOT}/scripts
-cd .repo/repo
-git pull origin stable
-cd ../../
-
 repo sync -j64 -vv --fail-fast
 
 scripts/kernel-metadata-regen-lineage.sh
@@ -16,20 +12,12 @@ popd
 
 # Sync linux and linux-stable kernels
 pushd ${MIRROR_ROOT}/kernel/linux
-cd .repo/repo
-git pull origin stable
-cd ../../
-
 repo sync -vv --fail-fast torvalds/linux
 repo sync -vv --fail-fast stable/linux
 popd
 
 # Sync google kernels
 pushd ${MIRROR_ROOT}/kernel/aosp
-cd .repo/repo
-git pull origin stable
-cd ../../
-
 repo sync -vv --fail-fast kernel/common
 
 repo sync -j64 -vv --fail-fast
@@ -38,20 +26,12 @@ popd
 # Sync qcom kernels
 pushd ${MIRROR_ROOT}/kernel/caf
 # TODO: fix this once repo supports refs removal in git config
-#cd .repo/repo
-#git pull origin stable
-#cd ../../
-
 #repo sync -j64 -vv --fail-fast
 ls -d kernel/*.git | parallel -j64 git -C {} fetch clo
 popd
 
 # Sync AOSP mirror
 pushd ${MIRROR_ROOT}/aosp
-cd .repo/repo
-git pull origin stable
-cd ../../
-
 repo sync -j64 -vv --fail-fast
 popd
 
@@ -62,10 +42,6 @@ scripts/kernel-mirror-to-lineage.sh
 popd
 
 pushd ${MIRROR_ROOT}/lineage
-cd .repo/repo
-git pull origin stable
-cd ../../
-
 repo sync -j64 -vv --fail-fast --no-clone-bundle
 popd
 
@@ -76,26 +52,14 @@ scripts/kernel-mirror-to-calyx.sh
 popd
 
 pushd ${MIRROR_ROOT}/calyx
-cd .repo/repo
-git pull origin stable
-cd ../../
-
 repo sync -j64 -vv --fail-fast --no-clone-bundle
 popd
 
 # Sync proprietary files mirrors
 pushd ${MIRROR_ROOT}/muppets
-cd .repo/repo
-git pull origin stable
-cd ../../
-
 repo sync -j64 -vv --fail-fast --no-clone-bundle
 popd
 
 pushd ${MIRROR_ROOT}/the-muppets
-cd .repo/repo
-git pull origin stable
-cd ../../
-
 repo sync -j64 -vv --fail-fast --no-clone-bundle
 popd
