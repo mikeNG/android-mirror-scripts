1593d5012bb043823738a82cb1743c5ee998671c
Michael Bestas <mikeioannina@gmail.com>
2023-12-01 20:21:51 +0200

    Allow custom job amount in sync.sh

--- sync.sh
+++ sync.sh
@@ -5,9 +5,21 @@ MIRROR_ROOT="${SCRIPT_PATH}/../.."
 
 export REPO_TRACE=0
 
+# Default to repo sync -j64
+SYNC_JOBS=64
+
+while [ "${#}" -gt 0 ]; do
+    case "${1}" in
+        -j | --jobs )
+                SYNC_JOBS="${2}";
+                ;;
+    esac
+    shift
+done
+
 # Sync the scripts and required manifests
 pushd ${MIRROR_ROOT}/scripts
-repo sync -j64 -vv --fail-fast
+repo sync -j"${SYNC_JOBS}" -vv --fail-fast
 
 scripts/kernel-metadata-regen-lineage.sh
 scripts/kernel-metadata-regen-calyx.sh
@@ -23,19 +35,19 @@ popd
 pushd ${MIRROR_ROOT}/kernel/aosp
 repo sync -vv --fail-fast kernel/common
 
-repo sync -j64 -vv --fail-fast
+repo sync -j"${SYNC_JOBS}" -vv --fail-fast
 popd
 
 # Sync qcom kernels
 pushd ${MIRROR_ROOT}/kernel/caf
 # TODO: fix this once repo supports refs removal in git config
-#repo sync -j64 -vv --fail-fast
-ls -d kernel/*.git | parallel -j64 git -C {} fetch clo
+#repo sync -j"${SYNC_JOBS}" -vv --fail-fast
+ls -d kernel/*.git | parallel -j"${SYNC_JOBS}" git -C {} fetch clo
 popd
 
 # Sync AOSP mirror
 pushd ${MIRROR_ROOT}/aosp
-repo sync -j64 -vv --fail-fast
+repo sync -j"${SYNC_JOBS}" -vv --fail-fast
 popd
 
 # Sync LineageOS mirror and set up alternates to AOSP and kernel mirrors
@@ -45,7 +57,7 @@ scripts/kernel-mirror-to-lineage.sh
 popd
 
 pushd ${MIRROR_ROOT}/lineage
-repo sync -j64 -vv --fail-fast --no-clone-bundle
+repo sync -j"${SYNC_JOBS}" -vv --fail-fast --no-clone-bundle
 popd
 
 # Sync CalyxOS mirror and set up alternates to AOSP and kernel mirrors
@@ -55,14 +67,14 @@ scripts/kernel-mirror-to-calyx.sh
 popd
 
 pushd ${MIRROR_ROOT}/calyx
-repo sync -j64 -vv --fail-fast --no-clone-bundle
+repo sync -j"${SYNC_JOBS}" -vv --fail-fast --no-clone-bundle
 popd
 
 # Sync proprietary files mirrors
 pushd ${MIRROR_ROOT}/muppets
-repo sync -j64 -vv --fail-fast --no-clone-bundle
+repo sync -j"${SYNC_JOBS}" -vv --fail-fast --no-clone-bundle
 popd
 
 pushd ${MIRROR_ROOT}/the-muppets
-repo sync -j64 -vv --fail-fast --no-clone-bundle
+repo sync -j"${SYNC_JOBS}" -vv --fail-fast --no-clone-bundle
 popd
