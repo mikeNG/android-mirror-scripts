9bcc570b598e65ab6ff0d8f5734364bf4f3114ce
Michael Bestas <mkbestas@gmail.com>
2026-06-15 01:06:51 +0300

    Ignore more kernels again and again

--- kernel-metadata-regen-lineage.sh
+++ kernel-metadata-regen-lineage.sh
@@ -50,6 +50,7 @@ kernels=`grep kernel ${MIRROR_MANIFEST}/default.xml | grep -v \
 -e 'qcs8550-devicetrees' \
 -e 'sm6475-devicetrees' \
 -e 'sm7435-devicetrees' \
+-e 'sm7635-devicetrees' \
 -e 'sm8450-devicetrees' \
 -e 'sm8475-devicetrees' \
 -e 'sm8550-devicetrees' \
@@ -60,6 +61,7 @@ kernels=`grep kernel ${MIRROR_MANIFEST}/default.xml | grep -v \
 -e 'qcs8550-modules' \
 -e 'sm6475-modules' \
 -e 'sm7435-modules' \
+-e 'sm7635-modules' \
 -e 'sm8450-modules' \
 -e 'sm8475-modules' \
 -e 'sm8550-modules' \
@@ -67,6 +69,7 @@ kernels=`grep kernel ${MIRROR_MANIFEST}/default.xml | grep -v \
 -e 'sm8650-modules' \
 -e 'sm8750-modules' \
 -e '-ack' \
+-e 'ayn_common-modules' \
 | sed -e 's#  <project name="LineageOS/##g' -e 's#" />##g'`
 
 echo -e "declare -A kernel_map\n" > ${KERNEL_MIRROR_MANIFEST}/lineage-metadata
